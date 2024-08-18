b6a560ff55041484e04f78e4d65ac4d8b43216cf
PlasmolysisMango <mrgrandpa@163.com>
2024-08-18 15:44:27 +0800

    init

---
+++ component/const.py
@@ -0,0 +1,12 @@
+import pygame
+
+WHITE = pygame.Color('white')
+BLACK = pygame.Color('black')
+GREEN = pygame.Color('darkgreen')
+RED = pygame.Color('red')
+YELLOW = 255, 255, 0
+PURPLE = 138, 43, 226
+BLUE = 0, 0, 255
+GREY = 128, 138, 135
+BROWN = 244, 164, 95
+LINE_WIDTH = 2
--- component/draw.py
+++ component/draw.py
@@ -1,14 +1,26 @@
-import pygame
+import time
+
+from const import *
+
 
 class Draw:
     def __init__(self, rect):
         self.rect = pygame.Rect(rect[0], rect[1], rect[2], rect[3])
 
     def init_surface(self):
-        surface = pygame.Surface((self.rect.w + LINEWIDTH / 2, self.rect.h + LINEWIDTH / 2))
+        surface = pygame.Surface((self.rect.w + LINE_WIDTH / 2, self.rect.h + LINE_WIDTH / 2))
         surface.fill(WHITE)
-        pygame.draw.rect(surface, BLACK, (0, 0, self.rect.w, self.rect.h), LINEWIDTH)
+        pygame.draw.rect(surface, BLACK, (0, 0, self.rect.w, self.rect.h), LINE_WIDTH)
         return surface
+
 
 if __name__ == '__main__':
     pygame.init()
+    size = width, height = 800, 600
+    background = pygame.display.set_mode((width, height))
+    draw = Draw((0, 0, 500, 500))
+    s = draw.init_surface()
+    background.blit(s, (0, 0))
+    pygame.display.set_caption("Monopoly by PlasmolysisMango")
+    pygame.display.update()
+    time.sleep(5)
